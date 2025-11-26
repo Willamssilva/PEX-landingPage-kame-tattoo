9590b5fa6b733b2aeed32b7bd0672af63a724d62
Willamssilva <willams99silva@gmail.com>
2025-11-26 13:19:20 -0300

    pequenas alteracoes de uso

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,37 @@ window.addEventListener('scroll', function () {
     let header = document.querySelector('#header')
     header.classList.toggle('roll', window.scrollY > 20)
 })
+
+//scroll do nav--mobile removendo e aparecendo com a rolagem da tela
+let ultimoSrollY = 0
+const navMobile = document.querySelector('.menu--mobile')
+window.addEventListener('scroll', () => {
+    const scrollAtualY = window.scrollY
+    const fimDaPagina = (window.innerHeight + scrollAtualY) >= document.body.offsetHeight - 10;
+
+    if (scrollAtualY > ultimoSrollY && !fimDaPagina) {
+        navMobile.classList.add('menu--mobile--hidden')
+    } else {
+        navMobile.classList.remove('menu--mobile--hidden')
+    }
+    ultimoSrollY = scrollAtualY
+})
+
+//Scroll suave para liks de navegacao
+const navLinks = document.querySelectorAll('#menu ul a.menu__link');
+navLinks.forEach(links => {
+    links.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            const headerHeight = document.querySelector('header').offsetHeight;
+            const navMobHeight = document.querySelector('.nav--mobile').offsetHeight;
+
+            const targetPosition = target.offsetTop - headerHeight - 30
+            window.scrollTo({ top: targetPosition, behavior: 'smooth' })
+
+            const targetPositionNav = target.offsetTop - navMobHeight - 20
+            window.scrollTo({ top: targetPositionNav, behavior: 'smooth' })
+        }
+    })
+})
